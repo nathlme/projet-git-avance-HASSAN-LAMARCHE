d0fb4ba0cef9795bedc166f6ad4a7c7d677167e3
lanathan <nathan.lamarche@ynov.com>
2025-09-25 16:58:54 +0200

    feat(main) : add the Negative function

--- main.go
+++ main.go
@@ -10,7 +10,12 @@ func Square(n int) int {
 	return n * n
 }
 
+func Negative(n int) int {
+	return -n
+}
+
 func main() {
 	fmt.Println("le nombre final est : ", AddTwo(10))
 	fmt.Println("le nombre final est : ", Square(10))
+	fmt.Println("le nombre final est : ", Negative(100))
 }
