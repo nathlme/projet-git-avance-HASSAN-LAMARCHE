2a1316c57ec74aa93cd1fd9353f45acdf34e0059
lanathan <nathan.lamarche@ynov.com>
2025-09-25 16:55:54 +0200

    feat(main) : add the Square function

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ func AddTwo(n int) int {
 	return n + 2
 }
 
+func Square(n int) int {
+	return n * n
+}
+
 func main() {
 	fmt.Println("le nombre final est : ", AddTwo(10))
+	fmt.Println("le nombre final est : ", Square(10))
 }
